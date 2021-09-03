5c48f8c89dbd419e801806f58fe5e94cba4f1c46
Casey Georgeson <caseys.georgeson@gmail.com>
2021-09-03 14:38:47 -0700

    Add last needed instructions

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 struct LMC {
-    int16_t mailboxes[100];
-    int16_t outbox;
-    int16_t accumulator;
+    uint16_t mailboxes[100];
+    uint16_t outbox;
+    uint16_t accumulator;
     uint8_t pc;
+    bool negative;
 };
 
 typedef enum {
@@ -97,20 +99,50 @@ int16_t numericCode(opcode_t op, uint16_t value) {
 
 void executeLMC(struct LMC* comp) {
     comp->pc = 0;
-    for(;comp->pc < 100; ++(comp->pc)) {
+    while(comp->pc < 100) {
+//        printf("(pc = %d)\n", comp->pc);
         Instruction_t current_instruction = decodeMailboxValue(comp->mailboxes[comp->pc]);
         switch(current_instruction.op) {
             case ADD:
                 comp->accumulator += comp->mailboxes[current_instruction.value];
+                comp->negative = false;
+                // todo: overflow
                 break;
             case SUB:
-                comp->accumulator -= comp->mailboxes[current_instruction.value];
+                if(comp->mailboxes[current_instruction.value] > comp->accumulator) {
+                    comp->negative = true;
+                } else {
+                    comp->accumulator -= comp->mailboxes[current_instruction.value];
+                }
                 break;
             case STA:
                 comp->mailboxes[current_instruction.value] = comp->accumulator;
                 break;
             case LDA:
                 comp->accumulator = comp->mailboxes[current_instruction.value];
+                break;
+            case BRA:
+                comp->pc = current_instruction.value;
+                continue;
+                break;
+            case BRZ:
+                if(comp->accumulator == 0 && !(comp->negative)) {
+                    comp->pc = current_instruction.value;
+                    continue;
+                }
+                break;
+            case BRP:
+                if(!(comp->negative)) {
+                    comp->pc = current_instruction.value;
+                    continue;
+                }
+                break;
+            case INP:
+                char input_string[6];
+                printf(": ");
+                gets(input_string);
+                uint16_t input_num = atoi(input_string);
+                comp->accumulator = input_num;
                 break;
             case OUT:
                 comp->outbox = comp->accumulator;
@@ -120,21 +152,23 @@ void executeLMC(struct LMC* comp) {
             default:
                 exit(0);
         }
+        ++(comp->pc);
+
     }
 }
 
 int main() {
     struct LMC comp = lmcInit();
 
-    comp.mailboxes[0] = numericCode(LDA, 99);
-    comp.mailboxes[1] = numericCode(SUB, 98);
+    comp.mailboxes[0] = numericCode(INP, 0);
+    comp.mailboxes[1] = numericCode(SUB, 99);
     comp.mailboxes[2] = numericCode(OUT, 0);
-    comp.mailboxes[3] = numericCode(SUB, 98);
-    comp.mailboxes[4] = numericCode(OUT, 0);
+    comp.mailboxes[3] = numericCode(BRZ, 5);
+    comp.mailboxes[4] = numericCode(BRA, 1);
+    comp.mailboxes[5] = numericCode(HLT, 0);
 
-
-    comp.mailboxes[98] = 2;
-    comp.mailboxes[99] = 10;
+    comp.mailboxes[98] = 10;
+    comp.mailboxes[99] = 1;
 
     for(int i = 0; i < 100; ++i) {
         Instruction_t ins = decodeMailboxValue(comp.mailboxes[i]);
